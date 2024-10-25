6ab3f90b66a3a5e3ef1e57d9e4fffddef9bbcd84
peLuis123 <pedrorc2018@gmail.com>
2024-10-24 21:31:08 -0500

    feat: se creo los controladores, las rutas y helper para autenticacion con cookies

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+const User = require('../models/user.model');
+const ApiError = require('../utils/apiError');
+const { catchAsync } = require('../utils/catchAsync');
+const { createSendToken } = require('../helpers/auth.helper');
+exports.signup = catchAsync(async (req, res, next) => {
+    const { name, email, password, rol } = req.body;
+    const newUser = await User.create({ name, email, password, rol });
+    createSendToken(newUser, 201, res);
+});
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new ApiError(400, 'Por favor, ingrese correo y contraseña');
+    }
+    const user = await User.findOne({ email }).select('+password');
+    if (!user || !(await user.matchPassword(password, user.password))) {
+        throw new ApiError(401, 'Por favor, ingrese correo y contraseña');
+    }
+    createSendToken(user, 200, res);
+});
+exports.logout = (req, res) => {
+    res.clearCookie('token', { path: '/' });
+    res.status(200).json({ message: 'Logged out successfully' });
+};
---
+++ src/helpers/auth.helper.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const signToken = (id) => { 
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '1d',
+    });
+};
+exports.createSendToken = (user, statusCode, res) => {
+   
+    const token = signToken(user._id);
+    const cookieOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+        path: '/',
+        maxAge: 24 * 60 * 60 * 1000,
+    };
+    res.cookie('token', token, cookieOptions);
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user,
+        },
+    });
+};
