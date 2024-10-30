706cbf0872342a60d4436318d287e527d1957d76
peLuis123 <pedrorc2018@gmail.com>
2024-10-29 23:45:34 -0500

    feat: index route configuration

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
+
 const authRoutes = require('./auth.routes.js');
 const subscriptionRouter = require('./subscribers.routes.js');
+const videojuegoRouter = require('./videojuego.router.js');
+const bibliotecaRouter = require('./bliblioteca.routes.js');
+const userRouter = require('./user.routes.js');
 const appRouter = express.Router();
 appRouter.use('/auth', authRoutes);
 appRouter.use('/sub', subscriptionRouter)
+appRouter.use('/game', videojuegoRouter);
+appRouter.use('/library', bibliotecaRouter)
+appRouter.use('/user', userRouter)
+
 module.exports = appRouter
